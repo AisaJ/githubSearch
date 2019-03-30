9d72d9cf89f77a1327bff4dd66c398784b6e5e8d
Aisa <aisajemila@gmail.com>
2019-03-30 15:32:12 +0300

    Imported the UsersService into the github-search component and place it in the providers array.

--- src/app/github-search/github-search.component.ts
+++ src/app/github-search/github-search.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../display-profile/users.service';
 
 @Component({
   selector: 'app-github-search',
   templateUrl: './github-search.component.html',
+  providers: [UsersService],
   styleUrls: ['./github-search.component.css']
 })
 export class GithubSearchComponent implements OnInit {
