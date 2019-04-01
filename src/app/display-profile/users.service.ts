7e82ef8f49ec51e959f22778f0bbc95e8ed4bd9e
Aisa <aisajemila@gmail.com>
2019-04-01 16:08:39 +0300

    Added an ApiKey to the link fetching the github users and repositories infomation.

--- src/app/display-profile/users.service.ts
+++ src/app/display-profile/users.service.ts
@@ -5,12 +5,14 @@ import {Observable} from 'rxjs';
 import { Repositories } from '../repositories';
 import { environment } from '../../environments/environment';
 import {Profiles} from '../profiles';
+import { CoreEnvironment } from '@angular/core/src/render3/jit/compiler_facade_interface';
 
 
 @Injectable()
 export class UsersService {
 
-  baseURL:string="https://api.github.com/users/"
+  baseURL:string="https://api.github.com/users/";
+  apiKey=environment.apiKey;
 
   constructor(private http:HttpClient) {
     console.log("service is now ready!"); 
@@ -18,10 +20,10 @@ export class UsersService {
    }
 
    getRepositories(username:string):Observable<Repositories[]>{
-     return this.http.get<Repositories[]>(this.baseURL + username + '/repos')
+     return this.http.get<Repositories[]>(this.baseURL + username + '/repos'+this.apiKey)
    }
    getProfiles(username:string):Observable<Profiles[]>{
-    return this.http.get<Profiles[]>(this.baseURL + username)
+    return this.http.get<Profiles[]>(this.baseURL + username+this.apiKey)
   }
 
 
