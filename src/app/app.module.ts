f8942c367c655ba4f3096fcc6ec55e54de5d4ca9
Aisa <aisajemila@gmail.com>
2019-04-02 06:29:39 +0300

    Created a new component repos and imported the routing modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubSearchComponent } from './github-search/github-search.component';
 import { SearchComponent } from './search/search.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReposComponent } from './repos/repos.component';
 
-
+//Defining routes
+const routes:Routes=[
+  {path:"github-search",component:GithubSearchComponent},
+  {path:"repos",component:ReposComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
     GithubSearchComponent,
-    SearchComponent       
+    SearchComponent,
+    ReposComponent       
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [UsersService],
   bootstrap: [AppComponent]
